74cfeec04da7732b5c4774bb61d8a0e7c7b1a05e
araaishpaul <supersikhkid@gmail.com>
2020-07-11 22:45:42 -0400

    basic navbar

---
+++ script.js
@@ -0,0 +1,22 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+@font-face {
+  font-family: 'Fira Code';
+  src: url('FiraCode-Regular.otf') format('opentype');
+}
+
+code {
+    -webkit-font-feature-settings: "liga" on, "calt" on;
+    -webkit-font-smoothing: antialiased;
+    text-rendering: optimizeLegibility;
+    font-family: 'Fira Code';
+}
